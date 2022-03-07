11cdafa7c730d570cf3eb374784c731c84b9c055
oscar9181 <oscarokola17@gmail.com>
2022-03-07 08:54:05 +0300

    index.html

---
+++ js/js script.js
@@ -0,0 +1,15 @@
+
+
+function getAkanName() {
+
+let days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let namesOfMale= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwami"]
+let namesOfFemale=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+}
+let button=.document getElementById("button")
+
+
+
+    
+}
